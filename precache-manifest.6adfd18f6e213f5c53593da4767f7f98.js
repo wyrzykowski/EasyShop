045c94938af18fdddf7896a7dd81f6f734ee16cd
Karol Wyrzykowski <karol.wyrzykowski128@gmail.com>
2018-10-23 23:39:19 +0200

    Updates

---
+++ precache-manifest.6adfd18f6e213f5c53593da4767f7f98.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/EasyShop/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "0d57b655bfc16a58af38",
+    "url": "/EasyShop/static/js/runtime~main.0d57b655.js"
+  },
+  {
+    "revision": "da95dd313e568d9b7737",
+    "url": "/EasyShop/static/js/main.da95dd31.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/EasyShop/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "da95dd313e568d9b7737",
+    "url": "/EasyShop/static/css/main.9f1f8d66.chunk.css"
+  },
+  {
+    "revision": "73f62bea77c490f686f97e7a4c55c19c",
+    "url": "/EasyShop/index.html"
+  }
+];
